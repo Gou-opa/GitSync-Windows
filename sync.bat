fb3972d48a03d371d0451ea5a398c15279fe8927
Gou-opa <monneymrt1998@gmail.com>
2020-03-15 00:38:32 +0700

    Autho sync at Sun 03/15/2020  0:38:32.51

--- sync.bat
+++ sync.bat
@@ -3,6 +3,7 @@ set destination_folder="%~1"
 echo day la dest "%~1"
 echo Checking internet connection
 Ping 8.8.8.8 -n 1 -w 1000
+if errorlevel 0 (
 echo Connected
 set "string=%destination_folder%"
 set "str=start /B .\sync_worker.bat %string:;=" & start /B .\sync_worker.bat "%"
@@ -10,5 +11,7 @@ echo .
 echo "final str:%str%"
 echo .
 %str%
-
+) else (
+echo No internet connection
+)
 goto :eof
